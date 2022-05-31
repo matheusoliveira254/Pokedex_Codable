82df2aac0316abcf3e9766e142da5f8f5a0f7e56
Karl Pfister <kpfister@me.com>
2022-05-31 17:06:00 -0600

    Small updates

--- Pokedex_Codable/Pokedex_Codable/Controllers/Networking/ResultError.swift
+++ Pokedex_Codable/Pokedex_Codable/Controllers/Networking/ResultError.swift
@@ -9,22 +9,25 @@ import Foundation
 
 enum ResultError: LocalizedError {
     
-    case invalidURL(String)
+    case invalidURL(URL)
     case thrownError(Error)
     case noData
     case unableToDecode
+    case noUser
     
     
     var errorDescription: String? {
         switch self {
-        case .invalidURL:
-            return "Unable to reach the server. Please try again."
+        case .invalidURL(let url):
+            return "Unable to reach the server. Please try again.\(url)"
         case .thrownError(let error):
             return "Error: \(error.localizedDescription) -> \(error)"
         case .noData:
             return "The server responded with no data. Please try again."
         case .unableToDecode:
             return "The server responded with bad data. Please try again."
+        case .noUser:
+            return "Do you need to sign in?"
         }
     }
 }
